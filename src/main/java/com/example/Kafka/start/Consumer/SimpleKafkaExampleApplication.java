4eef34bfb0991e7142d67bda3e7144defba4ea32
Maxim <githubmax@ukr.net>
2023-11-08 15:29:48 +0200

    Add 2 cancel.

--- src/main/java/com/example/Kafka/start/Consumer/SimpleKafkaExampleApplication.java
+++ src/main/java/com/example/Kafka/start/Consumer/SimpleKafkaExampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
+
+
 //@EnableKafka
 @SpringBootApplication
 public class SimpleKafkaExampleApplication {
