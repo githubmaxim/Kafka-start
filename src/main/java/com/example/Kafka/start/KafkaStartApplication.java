6ba40b789174ece816aad36687273a41b0285a27
Maxim <githubmax@ukr.net>
2023-11-08 15:00:49 +0200

    New project for Kafka-start.

---
+++ src/main/java/com/example/Kafka/start/KafkaStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kafka.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kafka/start/KafkaStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kafka.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
