fa8bc97ccda06e25feb5cf63988f6745574c96af
Maxim <githubmax@ukr.net>
2023-11-08 15:31:47 +0200

    Add 1 row with "111111111111111111111111111".

--- src/main/java/com/example/Kafka/start/Consumer/SimpleKafkaExampleApplication.java
+++ src/main/java/com/example/Kafka/start/Consumer/SimpleKafkaExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
-
+// 11111111111111111111111111111111111111111111111111111111
 
 //@EnableKafka
 @SpringBootApplication
